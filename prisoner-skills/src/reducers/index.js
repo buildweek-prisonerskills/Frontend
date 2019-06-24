1a00d7287a36df0fc891662337b16bbc90c84391
Jason Aviles <sandman2k18@outlook.com>
2019-06-24 17:43:30 -0400

    export ed my made reducers

--- prisoner-skills/src/reducers/index.js
+++ prisoner-skills/src/reducers/index.js
@@ -1,3 +1,5 @@
 import { combineReducers } from 'redux';
-
-export default combineReducers({});
+import PrisonersData from './Prisoner'
+export default combineReducers({
+    prisoners:PrisonersData
+});
