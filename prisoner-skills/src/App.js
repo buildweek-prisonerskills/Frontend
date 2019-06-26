77c6f51182d38fe291bcdc92d0d1142fd13e1c80
Jason Aviles <sandman2k18@outlook.com>
2019-06-26 14:11:17 -0400

    updated

--- prisoner-skills/src/App.js
+++ prisoner-skills/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './App.scss';
 import Prisoners from './components/Prisoners'
 import AddPrisoner from './forms/prisoner/AddPrisoner'
+import UpdateInmates from './forms/prisoner/EditPrisoner'
+import {Route} from 'react-router-dom'
 function App() {
 	return (
 		<div className='App'>
 		<Prisoners/>
+		<Route
+            exact
+            path={`/inmates/:id`}
+            render={props => <UpdateInmates {...props} />}
+          />
 		<AddPrisoner/>
 		</div>
 	);
--- prisoner-skills/src/components/DeleteBtn.js
+++ prisoner-skills/src/components/DeleteBtn.js
@@ -2,11 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { Delete_Inmate } from "../actions/Prisoners";
 const DeleteButton = props => {
-  console.log("delete button",props.inmateIds);
+  console.log("delete button",props);
   return (
     <div>
       <button className="ui basic red button"
-        onClick={() =>{ props.Delete_Inmate(props.inmateIds);} }
+        onClick={() =>{ props.Delete_Inmate( props);} }
       >
         Delete
       </button>
